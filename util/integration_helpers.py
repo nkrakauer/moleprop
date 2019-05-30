8e7cd98a09cdfa95182984711b996d8196f59708
Nathaniel <krakauern@gmail.com>
2019-05-30 14:06:51 -0500

    dataset integration helper class

--- util/integration_helpers.py
+++ util/integration_helpers.py
@@ -1,11 +1,29 @@
-def get_canonical_smiles(string):
-    return 0
+def remove_duplicates(data):
+    duplicates = data[data.duplicated(subset='smiles')]
+    result = data.drop_duplicates(subset='smiles', keep=False)#[~duplicates]
+    #for each unique smiles that has duplicates
+    for smiles in data[data.duplicated(subset='smiles')]['smiles'].unique():
+        dup_rows = data.loc[data['smiles'] == smiles]
+        if dup_rows['flashpoint'].unique().shape[0] == 1:
+            # remove all but one
+            result = result.append(dup_rows.iloc[0], sort=False)
+        else:
+            if dup_rows['flashpoint'].std() < 5:
+                # add 1 back
+                result = result.append(dup_rows.iloc[0], sort=False)
+    return result  
 
-def farenheitToKelvin(num):
-    return 0
+def canoicalize_smiles(data):
+    for idx, row in data.iterrows():
+        m = Chem.MolFromSmiles(data.iloc[idx]['smiles'])
+        if m != None:
+            data.iloc[idx]['smiles'] = Chem.MolToSmiles(m)
+        else:
+            data.iloc[idx]['smiles'] = None
+    return data
 
-def celToKelvin(num):
-    return 0
+def kelvinToCelsius(temp):
+    return temp -  273.15
 
-def checkDuplicates(df):
-    return 0
+def celsiusToKelvin(temp):
+    return temp + 273.15
