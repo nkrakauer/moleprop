f97ed158a5ff8c4a31a406c8c8a2525539e33864
Xiaoyu Sun <36873376+SeanXiaoyuSun@users.noreply.github.com>
2019-06-05 17:11:07 -0500

    Added test files

---
+++ test files/test_LOG.py
@@ -0,0 +1,26 @@
+import workflow as wf
+import pandas as pd
+
+print("About to load")
+loader = wf.Loader
+data = loader.load(file_name = 'integrated_dataset.csv',data_dir = '/srv/home/xsun256/Moleprop/summer19')
+
+print("About to split")
+splitter = wf.Splitter
+indices,dataset = splitter.LOG(data, 'pubchem')
+
+args = {'nb_epoch': 80,
+        'batch_size': 50,
+        'n_tasks': 1,
+        'graph_conv_layers':[64,64],
+        'dense_layer_size': 256,
+#        'dropout': 0.0,
+        'mode': 'regression'}
+
+
+print("About to simulate")
+rms,mae,prediction,test_dataset = wf.Simulate.LOG_validation(dataset,indices, 'graphconv',model_args = args)
+
+print("===============FINAL RESULTS==================")
+print("RMSE = ", rms)
+print("MAE = ", mae)
