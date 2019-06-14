3a76981fd006011374cc335a941f990b7bdb7e5f
Xiaoyu Sun <36873376+SeanXiaoyuSun@users.noreply.github.com>
2019-06-14 13:39:14 -0500

    test file for LOG validation

---
+++ util/test_files/test_LOG/test_LOG.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append('../')     # change the path to your dir of workflow.py
+import workflow as wf
+import pandas as pd
+
+print("About to load")
+loader = wf.Loader
+# TODO: need to change name and dir to your local dataset name and path
+data = loader.load(file_name = '9k_dataset.csv',data_dir = '/srv/home/xsun256/Moleprop/summer19/datasets')
+
+print("About to split")
+splitter = wf.Splitter
+# TODO: change test_group to the group name you want to leave out
+indices,dataset = splitter.LOG(data, test_group = 'pubchem')
+
+'''
+args = {'nb_epoch': 80,
+        'batch_size': 50,
+        'n_tasks': 1,
+        'graph_conv_layers':[64,64],
+        'dense_layer_size': 256,
+#        'dropout': 0.0,           # for testing if this workflow tool can correctly use default dropout if it is not inputted
+        'mode': 'regression'}
+'''
+
+args = None
+print("About to conduct LOG_validation")
+scores,prediction,test_dataset = wf.Run.LOG_validation(dataset,indices, model = 'GC',model_args = args,metrics = ['AAD', 'RMSE', 'MAE', 'R2'])
+
+# printing out scores
+for key in scores:
+    print(key+" = "+str(scores[key]))
+
+# getting plots
+print("About to plot full data")
+txt = {'RMSE/STD': scores['RMSE']/test_dataset['flashpoint'].std(),
+       'RMSE': scores['RMSE'],
+       'MAE': scores['MAE'],
+       'R2': scores['R2'],
+       'AAD': scores['AAD']}
+wf.Plotter.parity_plot(prediction,test_dataset,plot_name = "LOG_puchem_parity", text = txt)
+wf.Plotter.residual_histogram(prediction,test_dataset,plot_name = "LOG_pubchem_residual", text = txt)
