dca7e8db536b962b814491f4dd22eaba95e10e1e
Xiaoyu Sun <36873376+SeanXiaoyuSun@users.noreply.github.com>
2019-07-03 13:17:02 -0500

    Added LOG_v back for paper comparisons

--- util/workflow.py
+++ util/workflow.py
@@ -398,7 +398,55 @@ class Run:
         #file.write(str(scores))
         file.close()
         return scores, cv_predictions, cv_test_datasets
+    
+    def LOG_validation(data,
+                       indices,
+                       model,
+                       model_args = None,
+                       metrics = None):
+        """
+        Conduct leave-out-group validation
+        """
 
+        if not (model == 'MPNN' or model == 'graphconv' or model == 'GC' or model == 'GraphConv' or model == 'weave'):
+            sys.exit("Only supports MPNN model and graphconv and weave model")
+        train_indices, test_indices = indices
+        train_set = data.iloc[train_indices]
+        test_set = data.iloc[test_indices]
+        train_set.to_csv('train_set.csv',index = False)
+        test_set.to_csv('test_set.csv',index = False)
+        Loader.getinfo(train_set, "LOG_Train")
+        Loader.getinfo(test_set, "LOG_Test")
+        if model == 'MPNN':
+            rms_score,mae_score,r2_score,pred = Model.MPNN(model_args, "train_set.csv", "test_set.csv")
+        elif model == 'GraphConv' or model == 'graphconv' or model == 'GC':
+            rms_score,mae_score,r2_score,pred = Model.graphconv(model_args,"train_set.csv", "test_set.csv")
+        elif model == 'weave':
+            rms_score,mae_score,r2_score,pred = Model.weave(model_args,"train_set.csv", "test_set.csv")
+        os.remove("train_set.csv")
+        os.remove("test_set.csv")
+        scores_all = {'RMSE':rms_score,
+                      'MAE': mae_score,
+                      'R2': r2_score,
+                      'AAD':(Run.getAAPD(test_set,pred))}
+        scores = dict()
+        if metrics == None:  # return default scores (RMSE and R2)
+            scores = {'RMSE':scores_all['RMSE'],
+                      'R2':scores_all['R2']}
+        else:
+            for m in metrics:
+                if not ( m == 'RMSE' or m == 'MAE' or m == 'AAD' or m == 'R2' or m == 'train'):
+                    sys.exit('only supports RMSE, MAE, AAD, AAE, and R2 and train')
+                scores[m] = scores_all[m]
+        outliers = Run.get_outliers(test_set, pred)
+        outliers.to_csv('outliers.csv')
+        file = open('FINAL_RESULT.txt', 'w')
+        for key in scores:
+            s = key + " = " + str(scores[key]) + "\n"
+            file.write(s)
+        file.close()
+        return scores, pred, test_set
+    
     def custom_validation(train_dataset,
                           test_dataset,
                           model, 
